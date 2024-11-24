6163033b5d75174f98d95d5b1f8650ba1e6925f9
luigidematteis <ldematteis123@gmail.com>
2024-11-24 16:32:09 +0200

    remove relative line number
    Signed-off-by: luigidematteis <ldematteis123@gmail.com>

--- lua/config/default.lua
+++ lua/config/default.lua
@@ -1,12 +1,11 @@
-vim.g.silencedQueryResult = true
-
 vim.g.mapleader = " "
 
 vim.opt.encoding = "utf-8"
 
 vim.opt.compatible = false
 vim.opt.hlsearch = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
+vim.opt.number = true
 vim.opt.laststatus = 2
 vim.opt.vb = true
 vim.opt.ruler = true
@@ -17,7 +16,7 @@ vim.opt.colorcolumn = "120"
 vim.opt.textwidth = 120
 vim.opt.mouse = "a"
 vim.opt.clipboard = "unnamed"
-vim.opt.scrollbind = false
+vim.opt.scrollbind = true
 vim.opt.wildmenu = true
 
 vim.opt.tabstop = 3
